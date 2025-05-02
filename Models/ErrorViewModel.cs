5745dc5d8f00b68f32a9ea15e09fa7acbdd658a2
Tshwaro-T <tshwarothothela1@gmail.com>
2025-05-02 08:46:20 +0200

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ASPNETCore_DB.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
